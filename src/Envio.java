494331ec915580b150cde2d75d2da64786aa7fe0
Emerson-Martinez-0516 <emartine895@gmail.com>
2026-08-23 21:24:22 -0600

    Creación de constructor

--- src/Envio.java
+++ src/Envio.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Envio {
+
+    private String codigo;
+    private String destinatario;
+    private double peso;
+
+    public Envio(String codigo, String destinatario, double peso){
+        this.codigo =  codigo;
+        this.destinatario = destinatario;
+        this.peso = peso;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+
 }
